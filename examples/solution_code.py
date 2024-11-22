bdf9b3894bc9ad8434d89eb68e6dee4bbf4962e5
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-11-22 14:49:17 -0500

    Update solution_code.py

--- examples/solution_code.py
+++ examples/solution_code.py
@@ -1,1 +1,34 @@
-# Solution Code
+# SOLUTION CODE
+# Project title
+
+# Activity 1
+# Comment
+# Comment
+
+
+
+
+# Activity 2
+# Comment
+# Comment
+
+
+
+
+# Activity 3
+# Comment
+# Comment
+
+
+
+
+# Activity 4
+# Comment
+# Comment
+
+
+
+
+# Part 5
+# Comment
+# Comment
